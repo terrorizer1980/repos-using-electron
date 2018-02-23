7fc4e3d70dc4660b5c8daa00bfe911119ca6742f
Zeke Sikelianos <zeke@sikelianos.com>
2018-02-23 11:36:20 -0800

    grab more than one repo!

--- graphql.js
+++ graphql.js
@@ -1,6 +1,9 @@
 require('dotenv-safe').load()
+require('make-promises-safe')
 
 const {GraphQLClient} = require('graphql-request')
+const fs = require('fs')
+const path = require('path')
 
 const client = new GraphQLClient('https://api.github.com/graphql', {
   headers: {
@@ -8,10 +11,32 @@ const client = new GraphQLClient('https://api.github.com/graphql', {
   }
 })
 
-const query = `{
-  repository(owner: "zeit", name: "hyper") {
-    id
-  }
-}`
+function buildQuery(fullName) {
+  const [owner, repo] = fullName.split('/')
+  return `{
+    repository(owner: "${owner}", name: "${repo}") {
+      id
+    }
+  }`
+}
 
-client.request(query).then(data => console.log(data))
+// const repoNames = [
+//   'zeit/hyper',
+//   'desktop/desktop',
+//   'beakerbrowser/beaker'
+// ]
+
+const repoNames = fs.readFileSync(path.join(__dirname, 'data/buffett/electron.txt'), 'utf8')
+  .split('\n')
+  .filter(Boolean)
+  .map(name => name.trim())
+  .slice(0, 3)
+
+function getRepo(fullName) {
+  return client.request(buildQuery(fullName))
+}
+
+Promise.all(repoNames.map(getRepo))
+  // same as repoNames.map(repoName => getRepo(repoName))
+  .then(repos => console.log('got them repos', repos))
+  
